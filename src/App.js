e464beb132be3d9523a9a3e1236abfd97a10e036
joesphchang <joesphschang@gmail.com>
2022-07-12 18:30:46 -0400

    connected each component to app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-
 import './App.css';
+import About from './components/About/About';
+import Contact from './components/Contact/Contact';
+import Footer from './components/Footer/Footer';
+import Hero from './components/Hero/Hero';
+import Navigation from './components/Navigation/Navigation';
+import Portfolio from './components/Portfolio/Portfolio';
 
 function App() {
   return (
     <div className="App">
-
+      <Navigation />
+      <Hero />
+      <About />
+      <Portfolio />
+      <Contact />
+      <Footer />
     </div>
   );
 }
